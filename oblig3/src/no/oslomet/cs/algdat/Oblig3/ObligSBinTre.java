07cd9689dfdc13ee8a66ee38652e507256498f6d
felix <felixfotball99@gmail.com>
2019-11-03 17:40:26 +0100

    --> fjernet main fra obligSBinTre filen, siden det ble lagd en egen fil for main

--- oblig3/src/no/oslomet/cs/algdat/Oblig3/ObligSBinTre.java
+++ oblig3/src/no/oslomet/cs/algdat/Oblig3/ObligSBinTre.java
@@ -283,13 +283,5 @@ public class ObligSBinTre<T> implements beholder<T>
 
     } // BladnodeIterator
 
-    public static void main(String[] args){
-        Integer[] a = {4,7,2,9,5,10,8,1,3,6};
-
-        ObligSBinTre<Integer> tre = new ObligSBinTre<>(Comparator.naturalOrder());
-        for (int verdi : a) tre.leggInn(verdi);
-        System.out.println(tre.antall());
-
-    }
 
 } // ObligSBinTre
